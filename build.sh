3d0f41b4e07a8448218b3c3c3e5483edcbd9d1f5
iaroki <iaroki@protonmail.com>
2018-05-14 15:43:55 +0300

    add binary compression

--- build.sh
+++ build.sh
@@ -3,3 +3,5 @@
 docker build -t deezerjs-build .
 docker run -it --rm --name=deezerjs-build -v $PWD:/home/node/deezerjs deezerjs-build
 docker rmi deezerjs-build
+tar cvzf deezerjs-linux-x64.tar.gz deezerjs-linux-x64
+rm -rf deezerjs-linux-x64
