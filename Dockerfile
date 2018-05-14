381fa7bdf3601afdc4515bf6e77b16a28475e838
iaroki <iaroki@protonmail.com>
2018-05-14 15:34:07 +0300

    fix packager

--- Dockerfile
+++ Dockerfile
@@ -4,4 +4,4 @@ RUN npm install electron-packager -g
 USER node
 ENV DEEZDIR=/home/node/deezerjs
 WORKDIR $DEEZDIR
-CMD ["build.sh"]
+CMD ["electron-packager . deezerjs --platform=linux --arch=x64"]
--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 docker build -t deezerjs-build .
-docker run -it --rm --name=deezerjs-build -v $PWD:/home/node deezerjs-build
+docker run -it --rm --name=deezerjs-build -v $PWD:/home/node/deezerjs deezerjs-build
+docker rmi deezerjs-build
